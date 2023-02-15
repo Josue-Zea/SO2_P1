ea4f676b28a022f0d70f4121282d54b176a785ac
Zea <jdzeaherrera@gmail.com>
2023-02-14 18:24:35 -0600

    Probando wails

--- app/main.go
+++ app/main.go
@@ -2,8 +2,7 @@ package main
 
 import (
 	"embed"
-	"context"
-	"fmt"
+
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
@@ -11,27 +10,6 @@ import (
 
 //go:embed all:frontend/dist
 var assets embed.FS
-
-// App struct
-type App struct {
-	ctx context.Context
-}
-
-// NewApp creates a new App application struct
-func NewApp() *App {
-	return &App{}
-}
-
-// startup is called when the app starts. The context is saved
-// so we can call the runtime methods
-func (a *App) startup(ctx context.Context) {
-	a.ctx = ctx
-}
-
-// Greet returns a greeting for the given name
-func (a *App) Greet(name string) string {
-	return fmt.Sprintf("Hello %s, It's show time!", name)
-}
 
 func main() {
 	// Create an instance of the app structure
