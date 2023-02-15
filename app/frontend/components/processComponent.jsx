baed8c7f17d64f3d2cf9e18281fa9004f59a2113
Zea <jdzeaherrera@gmail.com>
2023-02-14 23:03:30 -0600

    Agregando funcionalidad de grafica uso de cpu

---
+++ app/frontend/components/processComponent.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Line } from 'react-chartjs-2';
+import Chart from 'chart.js/auto';
+
+const options = {
+    type: 'line',
+    scales: {
+        y: {
+            title: {
+                color: 'blue',
+                display: true,
+                text: 'Uso'
+            }
+        },
+        x: {
+            title: {
+                color: 'blue',
+                display: true,
+                text: 'Segundos'
+            }
+        }
+    }
+}
+export const Graphic = ({ dataCpu }) => {
+    console.log(dataCpu);
+    return (
+        <div className='BackgoundGraphic'>
+            <Line
+                datasetIdKey='id'
+                data={
+                    {
+                        labels: ["0", "2", "4", "6", "8", "10", "12", "14", "16", "18", "20", "22", "24", "26", "28", "30", "32", "34", "36", "38", "40", "42", "44", "46", "48", "50", "52", "54", "56", "58", "60"],
+                        datasets: [
+                            {
+                                id: 1,
+                                label: '%CPU',
+                                data: dataCpu,
+                                borderColor: 'rgb(255, 99, 132)',
+                                backgroundColor: 'rgba(255, 99, 132, 0.5)',
+                            }
+                        ],
+                        options: options
+                    }}
+            />
+        </div>
+    )
+}
--- app/frontend/src/CpuScreen.jsx
+++ app/frontend/src/CpuScreen.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Graphic } from '../components/processComponent';
 
 export const CpuScreen = ({setPage}) => {
+    const [data, setData] = useState([]);
+    
+    useEffect(() => {
+        setInterval(() => {
+            const newValue = Math.floor(Math.random() * (100 - 0 + 1)) + 0;
+            setData(prevData => [...prevData, newValue]);
+            if([...data, newValue].length > 30 ) deleteFirst();
+        }, 2000);
+    }, []);
+
+    const deleteFirst = () => {
+        data.shift();
+        setData(data);
+    };
+
     return (
         <>
-           <h1>CPU</h1>
+           <Graphic dataCpu={data}/>
            <div>
                 <button onClick={() => setPage(0) }>Regresar</button>
             </div>
