c6ad1ffbdeb44474d1d7f888789cdcebf231c1a8
Zea <jdzeaherrera@gmail.com>
2023-02-14 19:28:43 -0600

    Cambio en logo

--- app/frontend/src/App.jsx
+++ app/frontend/src/App.jsx
@@ -1,6 +1,4 @@
 import {useState} from 'react';
-import logo from './assets/images/logo-universal.png';
-import './App.css';
 import {Greet} from "../wailsjs/go/main/App";
 
 function App() {
@@ -15,20 +13,6 @@ function App() {
 
     return (
         <div id="App">
-            <div className="card mb-3 Card">
-                <div className="row g-0">
-                    <div className="col-md-4">
-                        <img src="..." className="img-fluid rounded-start" alt="..." />
-                    </div>
-                    <div className="col-md-8">
-                        <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
         </div>
     )
 }
