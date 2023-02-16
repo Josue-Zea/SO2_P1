94ba53ff61dac6b522afdf64ffdb747dadc49ac2
Zea <jdzeaherrera@gmail.com>
2023-02-16 12:21:49 -0600

    updating dependencies

--- app/frontend/vite.config.js
+++ app/frontend/vite.config.js
@@ -1,18 +1,7 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
-import reactChartjs2 from 'react-chartjs-2';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react-chartjs-2', 'chart.js/auto']
-    }
-  },
-  optimizeDeps: {
-    include: [
-      'react-chartjs-2',
-    ],
-  },
+  plugins: [react()]
 })
